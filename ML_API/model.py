0828ce98758e61e295d414bc4f9c076878a63a89
mohamed <hachami.mohamed20@gmail.com>
2024-10-26 01:17:20 +0200

    Initial commit

---
+++ ML_API/model.py
@@ -0,0 +1,16 @@
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+iris = load_iris(
+
+
+)
+X, y = iris.data, iris.target
+
+model = RandomForestClassifier()
+
+model.fit(X,y)
+
+joblib.dump(model ,'ML_API\model.joblib')
+
